5996f0d1b3756a5cdae528489111e0889e19a3fa
qnewmk <30813629+qnewmk@users.noreply.github.com>
2017-10-27 12:15:58 -0200

    Delete alunos.py

--- trabalho python bd/alunos.py
+++
@@ -1,8 +0,0 @@
-class Aluno:
-    def __init__(self):
-        self.sigla_curso = ''
-        self.matriculas = [] #aqui deve fazer uma lista de matriculas
-
-    def disciplinas_aluno (self):
-        # não tenho certeza se é isso
-        # return self.disciplinas
